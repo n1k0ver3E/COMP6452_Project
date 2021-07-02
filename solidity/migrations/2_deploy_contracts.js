af5b314d853a31a97f15bb654db21afa2a28dc58
Rittisak Kwanda <rittisak.kwanda@gmail.com>
2021-07-03 00:22:07 +1000

    Remove MetaCoin from deployment.

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -3,8 +3,8 @@ const MetaCoin = artifacts.require("MetaCoin");
 const Product = artifacts.require("Product");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
   deployer.deploy(Product);
 };
