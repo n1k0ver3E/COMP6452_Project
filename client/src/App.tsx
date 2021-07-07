b963b8abf3f9e032dcec90336c2abff8349cefa1
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-07 21:36:16 +1000

    Got React app to hook up to Profile contract

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Contract } from 'web3-eth-contract';
+import json from './contracts/Profile.json';
+import useWeb3 from './hooks/web3';
 
-function App() {
+const App: React.VFC = () => {
+  const { isLoading, isWeb3, web3, accounts } = useWeb3();
+  const [contract, setContract] = useState<Contract>();
+  const [value, setValue] = useState('');
+
+  const abi: any = json.abi;
+
+  useEffect(() => {
+    (async() => {
+      if(web3 !== null) {
+        // const networkId = await web3.eth.net.getId();
+        const deployedNetwork = json.networks[5777];
+        const instance = new web3.eth.Contract(
+          abi,
+          deployedNetwork && deployedNetwork.address
+        );
+        setContract(instance);
+      }
+    })();
+  }, [isLoading, isWeb3]);
+
+  console.log('CONTRACT', contract)
+
   return (
     <div className="App">
-      <h1>FoodieChain</h1>
+      <h1>Foodie Chain</h1>
     </div>
   );
 }
 
 export default App;
+
--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,10 +1,14 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
 const Product = artifacts.require("Product");
+const Profile = artifacts.require("Profile")
 
 module.exports = function(deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
   // deployer.deploy(MetaCoin);
-  deployer.deploy(Product);
+
+  // NOTE: Please ensure to enter 2 parameters when deploying Profile contract
+  // deployer.deploy(Profile, <address>, <regulator_name>);
+  deployer.deploy(Product)
 };
