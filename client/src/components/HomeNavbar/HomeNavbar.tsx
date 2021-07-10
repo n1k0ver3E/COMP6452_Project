7cb6b92a1abcdf235c11929eaafacac821783dbb
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-11 09:15:59 +1000

    fc-30 implement homenavbar for regulator and participant

--- client/src/components/HomeNavbar/HomeNavbar.tsx
+++ client/src/components/HomeNavbar/HomeNavbar.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react'
+import Logo from '../../assets/logo.jpg'
+import './homenavbar.css'
 
 type userType = 'regulator' | 'participant'
 
@@ -7,10 +9,44 @@ interface IProps {
 }
 
 const HomeNavbar: FC<IProps> = ({ type }) => {
+  // TODO - For mobile view, add a function to toggle menu bar
+
+  const isRegulator = type === 'regulator'
+  const isParticipant = type === 'participant'
+
   return (
-    <div>
-      <h1>HOME NAVBAR</h1>
-    </div>
+    <nav className="navbar is-white">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="brand-text" href="/">
+            <img src={Logo} alt={'Logo'} className="home-logo" />
+          </a>
+          <div className="navbar-burger burger" data-target="navMenu">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+        <div id="navMenu" className="navbar-menu">
+          <div className="navbar-start">
+            <a
+              className={isRegulator ? 'is-active navbar-item' : 'navbar-item'}
+              href="/regulator"
+            >
+              Regulator
+            </a>
+            <a
+              className={
+                isParticipant ? 'is-active navbar-item' : 'navbar-item'
+              }
+              href="/participant"
+            >
+              Participant
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- client/src/pages/Regulator/Regulator.tsx
+++ client/src/pages/Regulator/Regulator.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react'
+import HomeNavbar from '../../components/HomeNavbar'
 
 const Regulator: FC = () => {
   return (
     <div>
+      <HomeNavbar type={'regulator'} />
       <h1>Regulator Page</h1>
     </div>
   )
