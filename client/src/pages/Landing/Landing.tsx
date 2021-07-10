1926f2ceb844d3ac36b4c43dbd71b2e65b296f21
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-10 12:48:56 +1000

    fc-27 move fc logic to the Landing component

---
+++ client/src/pages/Landing/Landing.tsx
@@ -0,0 +1,44 @@
+import React, { FC, useEffect, useState } from 'react'
+import { Contract } from 'web3-eth-contract'
+import json from '../../contracts/Profile.json'
+import useWeb3 from '../../hooks/web3'
+
+const Landing: FC = () => {
+  const { isLoading, isWeb3, web3, accounts } = useWeb3()
+  const [contract, setContract] = useState<Contract>()
+  const [value, setValue] = useState('')
+
+  const abi: any = json.abi
+
+  useEffect(() => {
+    ;(async () => {
+      if (web3 !== null) {
+        // const networkId = await web3.eth.net.getId();
+        const deployedNetwork = json.networks[5777]
+        const instance = new web3.eth.Contract(
+          abi,
+          deployedNetwork && deployedNetwork.address
+        )
+        setContract(instance)
+      }
+    })()
+  }, [isLoading, isWeb3])
+
+  console.log('CONTRACT', contract)
+
+  const example = async () => {
+    const regAddr = await contract?.methods.regulatorAddress().call()
+    setValue(regAddr)
+  }
+
+  return (
+    <div className="App">
+      <h1>Foodie Chain</h1>
+      <div>The stored value is: {value}</div>
+      <p>Click here to run the contract↓</p>
+      <button onClick={example}>click</button>
+    </div>
+  )
+}
+
+export default Landing
