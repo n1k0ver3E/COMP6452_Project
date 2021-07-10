29e90da46d1c20fa7e8175a3105af4f66219cead
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-10 12:41:44 +1000

    fc-27 configure linting rules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { Contract } from 'web3-eth-contract';
-import json from './contracts/Profile.json';
-import useWeb3 from './hooks/web3';
+import React, { useEffect, useState } from 'react'
+import { Contract } from 'web3-eth-contract'
+import json from './contracts/Profile.json'
+import useWeb3 from './hooks/web3'
 
 const App: React.VFC = () => {
-  const { isLoading, isWeb3, web3, accounts } = useWeb3();
-  const [contract, setContract] = useState<Contract>();
-  const [value, setValue] = useState('');
+  const { isLoading, isWeb3, web3, accounts } = useWeb3()
+  const [contract, setContract] = useState<Contract>()
+  const [value, setValue] = useState('')
 
-  const abi: any = json.abi;
+  const abi: any = json.abi
 
   useEffect(() => {
-    (async() => {
-      if(web3 !== null) {
+    ;(async () => {
+      if (web3 !== null) {
         // const networkId = await web3.eth.net.getId();
-        const deployedNetwork = json.networks[5777];
+        const deployedNetwork = json.networks[5777]
         const instance = new web3.eth.Contract(
           abi,
           deployedNetwork && deployedNetwork.address
-        );
-        setContract(instance);
+        )
+        setContract(instance)
       }
-    })();
+    })()
   }, [isLoading, isWeb3])
 
   console.log('CONTRACT', contract)
 
-  const example = async() => {
-    const regAddr = await contract?.methods.regulatorAddress().call();
-    setValue(regAddr);
+  const example = async () => {
+    const regAddr = await contract?.methods.regulatorAddress().call()
+    setValue(regAddr)
   }
 
   return (
@@ -36,10 +36,9 @@ const App: React.VFC = () => {
       <h1>Foodie Chain</h1>
       <div>The stored value is: {value}</div>
       <p>Click here to run the contract↓</p>
-      <button onClick={example} >click</button>
+      <button onClick={example}>click</button>
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
--- client/src/hooks/web3.ts
+++ client/src/hooks/web3.ts
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
-import Web3 from 'web3';
-import getWeb3 from '../utils/getWeb3';
+import { useState, useEffect } from 'react'
+import Web3 from 'web3'
+import getWeb3 from '../utils/getWeb3'
 
 type state = {
-  isLoading: boolean;
-  isWeb3: boolean;
-  web3: Web3 | null;
-  accounts: string[];
-};
+  isLoading: boolean
+  isWeb3: boolean
+  web3: Web3 | null
+  accounts: string[]
+}
 
 const Hooks = (): state => {
   const [state, setState] = useState<state>({
@@ -15,30 +15,30 @@ const Hooks = (): state => {
     isWeb3: false,
     web3: null,
     accounts: [],
-  });
+  })
 
   useEffect(() => {
-    (async(): Promise<void> => {
+    ;(async (): Promise<void> => {
       try {
-        const web3: Web3 = await getWeb3();
-        const accounts = await web3.eth.getAccounts();
+        const web3: Web3 = await getWeb3()
+        const accounts = await web3.eth.getAccounts()
         setState({
           ...state,
           isLoading: false,
           isWeb3: true,
           web3,
           accounts,
-        });
+        })
       } catch {
         setState({
           ...state,
           isLoading: false,
-        });
+        })
       }
-    })();
-  }, []);
+    })()
+  }, [])
 
-  const { isLoading, isWeb3, web3, accounts } = state;
-  return { isLoading, isWeb3, web3, accounts };
-};
-export default Hooks;
+  const { isLoading, isWeb3, web3, accounts } = state
+  return { isLoading, isWeb3, web3, accounts }
+}
+export default Hooks
