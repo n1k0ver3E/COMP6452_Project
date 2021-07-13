c23dd640b3a2c2ed17d7eebb55e5c767131857c6
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-14 07:37:01 +1000

    fc-38 WIP

---
+++ client/src/components/ViewAccountForm/ViewAccountForm.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useContext } from 'react'
+import { ProfileContractAPIContext } from '../../contexts/ProfileContractAPI'
+import { IAccountTypeDropdown } from '../../interfaces/contract'
+
+const ViewAccountForm: FC = () => {
+  const { registeredAccounts } = useContext(ProfileContractAPIContext)
+
+  return (
+    <>
+      <h1 className="title is-4">Track your account progress</h1>
+      <form>
+        <div className="select is-fullwidth">
+          <select defaultValue={'DEFAULT'} name="accountType" id="accountType">
+            <option value={'DEFAULT'} disabled>
+              Select Account Type
+            </option>
+          </select>
+        </div>
+
+        <label className="checkbox">
+          <input type="checkbox" /> Account address not on the list
+        </label>
+      </form>
+    </>
+  )
+}
+
+export default ViewAccountForm
