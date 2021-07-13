ef10db8295b8eb61140b4ac1cd1b139a981bb232
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-14 07:37:01 +1000

    fc-38 WIP

--- client/src/pages/Participant/ViewAccount/ViewAccount.tsx
+++ client/src/pages/Participant/ViewAccount/ViewAccount.tsx
@@ -1,10 +1,23 @@
 import React, { FC } from 'react'
+import RegistrationImage from '../../../assets/registration.png'
+import ViewAccountForm from '../../../components/ViewAccountForm'
 
 const ViewAccount: FC = () => {
   return (
-    <div>
-      <h1>View Account</h1>
-    </div>
+    <section className="container has-background-light">
+      <div className="columns is-multiline">
+        <div className="column is-10 is-offset-2 register">
+          <div className="columns">
+            <div className="column left mt-6">
+              <ViewAccountForm />
+            </div>
+            <div className="column right has-text-centered">
+              <img src={RegistrationImage} alt="registration infographics" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
