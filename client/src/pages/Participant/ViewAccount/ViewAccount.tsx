0ac8bb4452defeadb33c157ba8a49fa8b39d1f88
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-14 22:17:49 +1000

    fc-38 adding logic to set form

--- client/src/pages/Participant/ViewAccount/ViewAccount.tsx
+++ client/src/pages/Participant/ViewAccount/ViewAccount.tsx
@@ -1,16 +1,34 @@
-import React, { FC } from 'react'
+import React, { FC, ChangeEvent, useState } from 'react'
 import ProfileTrackingImage from '../../../assets/database.png'
 import ViewAccountForm from '../../../components/ViewAccountForm'
 import './viewaccount.css'
+import { IViewAccountDetails } from '../../../interfaces/contract'
+
+const initialState: IViewAccountDetails = {
+  registeredAddress: '',
+  accountAddress: '',
+}
 
 const ViewAccount: FC = () => {
+  const [data, setData] = useState<IViewAccountDetails>(initialState)
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target
+
+    console.log(name, value)
+
+    setData({ ...data, [name]: value })
+  }
+
   return (
     <section className="container has-background-light">
       <div className="columns is-multiline">
         <div className="column is-10 is-offset-2 register">
           <div className="columns">
             <div className="column left mt-6">
-              <ViewAccountForm />
+              <ViewAccountForm handleChange={handleChange} />
             </div>
             <div className="column right has-text-centered">
               <img
