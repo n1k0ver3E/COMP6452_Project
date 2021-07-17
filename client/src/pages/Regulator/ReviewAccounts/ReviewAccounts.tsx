21a1d72b29c2ad23ce7e53f59bbc5bb9aa3a4a8c
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-18 09:49:46 +1000

    fc-7 add basic styling to structure

--- client/src/pages/Regulator/ReviewAccounts/ReviewAccounts.tsx
+++ client/src/pages/Regulator/ReviewAccounts/ReviewAccounts.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useContext } from 'react'
 import { ProfileContractAPIContext } from '../../../contexts/ProfileContractAPI'
 import AccountsTable from '../../../components/AccountsTable'
+import UserAccountsIcon from '../../../assets/user-accounts.png'
+import './reviewaccounts.css'
 
 const ReviewAccounts: FC = () => {
   const { pendingAccounts, approvedAccounts, rejectedAccounts } = useContext(
@@ -31,9 +33,36 @@ const ReviewAccounts: FC = () => {
   ]
 
   return (
-    <div>
-      <AccountsTable columns={COLUMNS} data={pendingAccounts} />
-    </div>
+    <section className="container">
+      <div className="column is-12">
+        <div className="title is-4">Accounts to Review</div>
+        <AccountsTable columns={COLUMNS} data={pendingAccounts} />
+      </div>
+
+      <div className="column is-12 mt-5">
+        <div className="columns">
+          <div className="column is-10">
+            <div>
+              <div className="title is-4">Rejected Accounts</div>
+              <AccountsTable columns={COLUMNS} data={rejectedAccounts} />
+            </div>
+
+            <div className="mt-6">
+              <div className="title is-4">Approved Accounts</div>
+              <AccountsTable columns={COLUMNS} data={approvedAccounts} />
+            </div>
+          </div>
+
+          <div className="column is-2 icon-wrapper">
+            <img
+              src={UserAccountsIcon}
+              alt={'user accounts icon'}
+              className="user-accounts"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
