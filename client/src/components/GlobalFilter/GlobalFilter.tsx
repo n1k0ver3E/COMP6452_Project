2a8c41c9e3fe1839e73614dc88d86736f67934fc
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-18 16:03:20 +1000

    fc-7 beautify the GlobalFilter component

--- client/src/components/GlobalFilter/GlobalFilter.tsx
+++ client/src/components/GlobalFilter/GlobalFilter.tsx
@@ -7,10 +7,24 @@ interface IGlobalFilterProps {
 
 const GlobalFilter: FC<IGlobalFilterProps> = ({ filter, setFilter }) => {
   return (
-    <span>
-      Search:{' '}
-      <input value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
-    </span>
+    <div className="container mb-5">
+      <div className="field">
+        <label className="label">
+          Search By Account Name Or Account Address
+        </label>
+        <div className="control has-icons-left has-icons-right">
+          <input
+            className="input"
+            type="text"
+            value={filter || ''}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-search" />
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
