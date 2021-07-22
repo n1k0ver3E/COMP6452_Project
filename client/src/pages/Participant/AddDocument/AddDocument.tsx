8f10020a60181aba567262b746faf6c56f1a1afa
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-22 20:43:23 +1000

    fc-44 implement switchable tabs for different types of documents

--- client/src/pages/Participant/AddDocument/AddDocument.tsx
+++ client/src/pages/Participant/AddDocument/AddDocument.tsx
@@ -1,10 +1,75 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { DocumentType } from '../../../enums/contract'
 
 const AddDocument: FC = () => {
+  const [profileDocumentsActiveClass, setProfileDocumentsActiveClass] =
+    useState<string>('is-active')
+  const [productDocumentsActiveClass, setProductDocumentsActiveClass] =
+    useState<string>('')
+  const [
+    traceabilityDocumentsActiveClass,
+    setTraceabilityDocumentsActiveClass,
+  ] = useState<string>('')
+
+  const switchTab = (documentType: number) => {
+    switch (documentType) {
+      case DocumentType.PROFILE:
+        setProfileDocumentsActiveClass('is-active')
+        setProductDocumentsActiveClass('')
+        setTraceabilityDocumentsActiveClass('')
+        break
+      case DocumentType.PRODUCT:
+        setProductDocumentsActiveClass('is-active')
+        setProfileDocumentsActiveClass('')
+        setTraceabilityDocumentsActiveClass('')
+        break
+      case DocumentType.TRACEABILITY:
+        setTraceabilityDocumentsActiveClass('is-active')
+        setProfileDocumentsActiveClass('')
+        setProductDocumentsActiveClass('')
+    }
+  }
+
   return (
-    <div>
-      <h1>Participant Add Document</h1>
-    </div>
+    <>
+      <div className="tabs is-centered is-boxed">
+        <ul>
+          <li
+            className={profileDocumentsActiveClass}
+            onClick={() => switchTab(DocumentType.PROFILE)}
+          >
+            <a>
+              <span className="icon is-small">
+                <i className="fas fa-users" aria-hidden="true" />
+              </span>
+              <span>Profile Documents</span>
+            </a>
+          </li>
+          <li
+            className={productDocumentsActiveClass}
+            onClick={() => switchTab(DocumentType.PRODUCT)}
+          >
+            <a>
+              <span className="icon is-small">
+                <i className="fas fa-box" aria-hidden="true" />
+              </span>
+              <span>Product Documents</span>
+            </a>
+          </li>
+          <li
+            className={traceabilityDocumentsActiveClass}
+            onClick={() => switchTab(DocumentType.TRACEABILITY)}
+          >
+            <a>
+              <span className="icon is-small">
+                <i className="fas fa-truck-moving" aria-hidden="true" />
+              </span>
+              <span>Traceability Documents</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
