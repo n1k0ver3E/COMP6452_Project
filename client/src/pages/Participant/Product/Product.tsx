ad6ee06d55fefe1fff320077d9d5db7c8bef031c
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-24 06:05:40 +1000

    fc-44 logic to switch between tabs

--- client/src/pages/Participant/Product/Product.tsx
+++ client/src/pages/Participant/Product/Product.tsx
@@ -1,11 +1,53 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { ProductCategory } from '../../../enums/contract'
 
 const Product: FC = () => {
+  const [farmerActiveClass, setFarmerActiveClass] =
+    useState<string>('is-active')
+  const [manufacturerActiveClass, setManufacturerActiveClass] =
+    useState<string>('')
+  const [retailActiveClass, setRetailActiveClass] = useState<string>('')
+  const [purchaseActiveClass, setPurchaseActiveClass] = useState<string>('')
+
+  const switchTab = (productCategory: number) => {
+    switch (productCategory) {
+      case ProductCategory.FARMING:
+        setFarmerActiveClass('is-active')
+        setManufacturerActiveClass('')
+        setRetailActiveClass('')
+        setPurchaseActiveClass('')
+        break
+      case ProductCategory.MANUFACTURING:
+        setManufacturerActiveClass('is-active')
+        setFarmerActiveClass('')
+        setRetailActiveClass('')
+        setPurchaseActiveClass('')
+        break
+      case ProductCategory.RETAILING:
+        setRetailActiveClass('is-active')
+        setManufacturerActiveClass('')
+        setFarmerActiveClass('')
+        setPurchaseActiveClass('')
+        break
+      case ProductCategory.PURCHASING:
+        setPurchaseActiveClass('is-active')
+        setRetailActiveClass('')
+        setManufacturerActiveClass('')
+        setFarmerActiveClass('')
+        break
+      default:
+        break
+    }
+  }
+
   return (
     <>
       <div className="tabs is-centered is-boxed">
         <ul>
-          <li>
+          <li
+            className={farmerActiveClass}
+            onClick={() => switchTab(ProductCategory.FARMING)}
+          >
             <a>
               <span className="icon is-small">
                 <i className="fas fa-seedling" aria-hidden="true" />
@@ -13,7 +55,10 @@ const Product: FC = () => {
               <span>Farmer</span>
             </a>
           </li>
-          <li>
+          <li
+            className={manufacturerActiveClass}
+            onClick={() => switchTab(ProductCategory.MANUFACTURING)}
+          >
             <a>
               <span className="icon is-small">
                 <i className="fas fa-industry" aria-hidden="true" />
@@ -21,7 +66,10 @@ const Product: FC = () => {
               <span>Manufacturer</span>
             </a>
           </li>
-          <li>
+          <li
+            className={retailActiveClass}
+            onClick={() => switchTab(ProductCategory.RETAILING)}
+          >
             <a>
               <span className="icon is-small">
                 <i className="fas fa-store-alt" aria-hidden="true" />
@@ -29,7 +77,10 @@ const Product: FC = () => {
               <span>Retail</span>
             </a>
           </li>
-          <li>
+          <li
+            className={purchaseActiveClass}
+            onClick={() => switchTab(ProductCategory.PURCHASING)}
+          >
             <a>
               <span className="icon is-small">
                 <i className="fas fa-credit-card" aria-hidden="true" />
