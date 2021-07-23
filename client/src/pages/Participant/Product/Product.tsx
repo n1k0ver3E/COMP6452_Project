f18791c7054fc3ea23087637784401b0fefb3d72
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-24 05:50:45 +1000

    fc-44 switchable tabs for product page

--- client/src/pages/Participant/Product/Product.tsx
+++ client/src/pages/Participant/Product/Product.tsx
@@ -1,74 +1,46 @@
-import React, { FC, ChangeEvent } from 'react'
-import ProfileTrackingImage from '../../../assets/database.png'
-import { ProfileContractContext } from '../../../contexts/ProfileContract'
-// import { ProductContractContext } from '../../../contexts/ProductContract'
-// import { ProductContractAPIContext } from '../../../contexts/ProductContractAPI'
-
+import React, { FC } from 'react'
 
 const Product: FC = () => {
-    return (
-      <section className="container has-background-light">
-        <div className="columns is-multiline">
-          <div className="column is-10 is-offset-2 register">
-            <div className="columns">
-              <div className="column left mt-6">
-              
-              <h1 className="title is-4">Add Product</h1>
-              <form className="mt-5">
-                <div className="field">
-                  <div className="control">
-                  <input
-                    className="input mt-4"
-                    id = "productName"
-                    name="productName"
-                    placeholder = "ProductName"
-                  />
-                  <input
-                    className="input mt-3 mb-4"
-                    id="productLocation"
-                    name="productLocation"
-                    placeholder = "productLocation"
-                  />
-                  <p className="description"><b>Planting Date</b></p>
-                  <input
-                    className="input mb-3"
-                    name = "plantingDate"
-                    id = "plantingDate"
-                    type="date"
-
-                  />
-                  
-                  <p className="description"><b>Harvest Date</b></p>
-                  <input
-                    className="input mb-3"
-                    name = "harvestDate"
-                    id = "harvestDate"
-                    type="date"
-                  />
-                  
-
-                  </div>
-                  
-                  <button 
-                    className='button is-block is-link is-fullwidth mt-3'>
-                    Add
-                  </button>
-                </div>
-              </form>
-              </div>
-  
-              <div className="column right has-text-centered">
-                <img
-                  src={ProfileTrackingImage}
-                  alt="profile tracking"
-                  className="side-image"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    )
-  }
+  return (
+    <>
+      <div className="tabs is-centered is-boxed">
+        <ul>
+          <li>
+            <a>
+              <span className="icon is-small">
+                <i className="fas fa-seedling" aria-hidden="true" />
+              </span>
+              <span>Farmer</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="icon is-small">
+                <i className="fas fa-industry" aria-hidden="true" />
+              </span>
+              <span>Manufacturer</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="icon is-small">
+                <i className="fas fa-store-alt" aria-hidden="true" />
+              </span>
+              <span>Retail</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="icon is-small">
+                <i className="fas fa-credit-card" aria-hidden="true" />
+              </span>
+              <span>Purchase</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
 
 export default Product
