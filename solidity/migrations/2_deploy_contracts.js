77e6d87d272bf97958236e5728c0ca22af0585a7
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-23 13:57:55 +1000

    Amend the way to deploy Profile contract

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 // const ConvertLib = artifacts.require("ConvertLib");
 // const MetaCoin = artifacts.require("MetaCoin");
-const Profile = artifacts.require("Profile")
+const Profile = artifacts.require("Profile");
+const Document = artifacts.require("Document");
 
 module.exports = function (deployer, network, accounts) {
   // deployer.deploy(ConvertLib);
@@ -10,7 +11,11 @@ module.exports = function (deployer, network, accounts) {
 
   // NOTE: Please ensure to enter 2 parameters when deploying Profile contract
   //deployer.deploy(Profile, <address>, <regulator_name>);
-  const profile = deployer.deploy(Profile, accounts[0], 'regulator');
-  deployer.deploy(Document, profile.address);
+  // const profile = deployer.deploy(Profile, accounts[0], "regulator");
+  // deployer.deploy(Document, profile.address);
+  deployer.deploy(Profile, accounts[0], "regulator").then((profile) => {
+    deployer.deploy(Document, profile.address);
+  });
+
   //deployer.deploy(Product)
 };
