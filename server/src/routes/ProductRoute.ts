c448440af1f0c1b0afaf992347d35ed3dd29aa2a
Niko Pang <pyf0311@gmail.com>
2021-07-25 13:03:04 +0800

    Update ProductRoute.ts

--- server/src/routes/ProductRoute.ts
+++ server/src/routes/ProductRoute.ts
@@ -5,8 +5,8 @@ const router = express.Router()
 
 router.route('/').post(ProductController.createProduct)
 router.route('/farming-info').post(ProductController.addProductFarmingInfo)
-router.route("/manu-info").put(ProductController.addProductFarmingInfo)
-router.route("/retailing-info").put(ProductController.addProductFarmingInfo)
-router.route("/purchasing-info").put(ProductController.addProductFarmingInfo)
+router.route("/manu-info").patch(ProductController.addProductFarmingInfo)
+router.route("/retailing-info").patch(ProductController.addProductFarmingInfo)
+router.route("/purchasing-info").patch(ProductController.addProductFarmingInfo)
 
 export default router
