8d112d28a97725f677164db3a37245ce247c0942
Promie Yutasane <promie.yutasane@gmail.com>
2021-07-27 07:26:11 +1000

    added a function to refresh user

--- client/src/components/CurrentActiveUser/CurrentActiveUser.tsx
+++ client/src/components/CurrentActiveUser/CurrentActiveUser.tsx
@@ -5,11 +5,15 @@ import './currentactiveuser.css'
 const CurrentActiveUser: FC = () => {
   const { accounts } = useContext(ProfileContractContext)
 
+  const refreshUser = () => {
+    window.location.reload()
+  }
+
   return (
     <div className="container current-active-user">
       <div>
         <span className="user">Active User: {accounts[0]}</span>
-        <span className="icon refresh-user">
+        <span className="icon refresh-user" onClick={refreshUser}>
           <i className="fas fa-sync-alt" />
         </span>
       </div>
