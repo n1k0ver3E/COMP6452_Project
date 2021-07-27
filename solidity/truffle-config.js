59754a242f99986a9a2a9f4a8d1bc7bcfb0e330a
Rittisak Kwanda <rittisak.kwanda@gmail.com>
2021-07-27 20:34:55 +1000

    ...

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -11,11 +11,13 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
+      //port: 9545,
       port: 7545,
       network_id: "*",
     },
     test: {
       host: "127.0.0.1",
+      //port: 9545,
       port: 7545,
       network_id: "*",
     },
