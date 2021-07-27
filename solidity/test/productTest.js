bb0c9a72f07e4f1f19bfe11ebb8dff1a4106fc25
pyf0311 <pyf0311@gmail.com>
2021-07-27 14:23:30 +0800

    Add documentTest

--- solidity/test/productTest.js
+++ solidity/test/productTest.js
@@ -36,17 +36,20 @@ contract('ProductSC', (accounts) => {
         await profile.approveAccount(logistic, 1, { from: regulator });
         await profile.registerAccount(oracle, "Oracle", 6, { from: oracle });
         await profile.approveAccount(oracle, 1, { from: regulator });
+        
+        productAId = await product.createProduct.call("Potato",123,345,"syd", { from: farmer });
+        await product.createProduct("Potato",1210,1230,"syd", { from: farmer });
     });
     
     
     // Test createProduct()
     it('createProduct() successful Test)', async () => {
-        const productId = await product.createProduct.call("Potato",123,345,"syd", { from: farmer });
-        await product.createProduct("Potato",123,345,"syd", { from: farmer });
+        const productId = await product.createProduct.call("Tomato",123,345,"syd", { from: farmer });
+        await product.createProduct("Tomato",1230,3455,"syd", { from: farmer });
 
-        assert.equal(productId, 1, "ProductID should be equal to 1");
-        const result = await product.products.call(1);
-        assert.equal(result.productName, "Potato", 'ProductName should be potato');
+        assert.equal(productId, 2, "ProductID should be equal to 1");
+        const result = await product.products.call(2);
+        assert.equal(result.productName, "Tomato", 'ProductName should be tomato');
     });
     
     it('createProduct() fail when account role is not farmer )', async () => {
@@ -69,6 +72,7 @@ contract('ProductSC', (accounts) => {
         const result = await product.products.call(1);
         const manu_detail = await product.manu_process.call(1);
         assert.equal(result.statusType.toNumber(), 1, 'Status should be 1 in Manufacturing process');
+        assert.equal(result.manufacturerId, manufacturer,"error");
         assert.equal(manu_detail.processType, "Potato Sause", "processType should be in array");
     });
     
@@ -103,14 +107,6 @@ contract('ProductSC', (accounts) => {
     });
     
     // Test purchasingProductInfo()
-    it('purchasingProductInfo() successful Test)', async () => {
-        let purchase = await product.purchasingProductInfo.call(1,30, { from: consumer });
-        await product.purchasingProductInfo(1,30, { from: consumer });
-
-        const result = await product.products.call(1);
-        // assert.equal(result.statusType.toNumber(), 4, 'Status should be 4 in purchasing process');
-        assert.equal(purchase_process.price, 30, "price should be in mapping collection.");
-    });
     
     it('purchasingProductInfo() fail when datatype is not match )', async () => {
         try {
@@ -124,17 +120,28 @@ contract('ProductSC', (accounts) => {
     
     // Test recallProduct()
     it('recallProduct() successful Test for reasonable role.)', async () => {
-        let recall = await product.recallProduct.call(1, { from: farmer });
-        await product.recallProduct(1,  { from: farmer });
+        const recall = await product.recallProduct(productAId, { from: farmer });
 
         truffleAssert.eventEmitted(recall, 'RecallProduct', (ev) => {
             return ev.productId == 1;
         });
         
-        const result = await product.products.call(1, { from: farmer });
+        const result = await product.products.call(1);
+
         assert.equal(result.statusType.toNumber(), 5, 'Status should be 5 in recalling process');
     });
     
+    it('recallProduct() Fail Test for unreasonable role.)', async () => {
+        try{
+            const recall = await product.recallProduct(productAId, { from: retailer });
+            const result = await product.products.call(1);
+            assert.equal(result.statusType.toNumber(), 5, 'Status should be 5 in recalling process');
+        }
+        catch(error){
+            assert(error,"error");
+            assert(error.message.includes("The address is not farmer,manufacturer, distributor, retailer or consumer"));
+        }
+    });
     
     
     
